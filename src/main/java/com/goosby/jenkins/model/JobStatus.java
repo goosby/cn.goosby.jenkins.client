b9478edd75db98e44657dc8b9b4c796c5774859d
goosby <goosby@outlook.com>
2015-12-17 23:41:57 +0800

    README.md

---
+++ src/main/java/com/goosby/jenkins/model/JobStatus.java
@@ -0,0 +1,33 @@
+package com.goosby.jenkins.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JobStatus {
+	public static enum Status {
+		OK,
+		WARNING,
+		FAILED;
+		
+		public static Status create(String colorCode) {
+			Map<String, Status> codes = new HashMap<String, Status>();
+			codes.put("blue", OK);
+			codes.put("yellow", WARNING);
+			codes.put("red", FAILED);
+			return codes.get(colorCode);
+		}
+	}
+	
+	String name;
+	Status status;
+	public JobStatus(String name, Status status) {
+		this.name = name;
+		this.status = status;
+	}
+	public String getName() {
+		return name;
+	}
+	public Status getStatus() {
+		return status;
+	}
+}
